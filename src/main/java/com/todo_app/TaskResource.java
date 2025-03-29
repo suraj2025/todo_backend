feac8b398b5a1a8dca0cc8b731bf5b4b7644b59a
Suraj Kumar <sk5195609@gmail.com>
2025-03-29 14:51:07 +0530

    Update TaskResource.java

--- src/main/java/com/todo_app/TaskResource.java
+++ src/main/java/com/todo_app/TaskResource.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/tasks")
-@CrossOrigin(origins = "https://todo-spring.vercel.app")
+@CrossOrigin(origins = "https://todo-spring.vercel.app/")
 public class TaskResource {
      @Autowired
      TaskRepo repo;
